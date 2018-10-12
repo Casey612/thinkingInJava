d7f3dcca0b088a9b6f2fbe868918f674c6454b56
yuki <cuiyuzhe@miaozhen.com>
2018-10-12 19:26:49 +0800

    [wip]ch 18.9

---
+++ src/java/code/ch18/ChangeSystemOut.java
@@ -0,0 +1,16 @@
+package code.ch18;
+
+import java.io.PrintWriter;
+
+/**
+ * @author yuzhe
+ * @since 10/12/18
+ */
+public class ChangeSystemOut {
+
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out, true);
+        out.println("hello, world");
+    }
+
+}
---
+++ src/java/code/ch18/OSExecuteException.java
@@ -0,0 +1,11 @@
+package code.ch18;
+
+/**
+ * @author yuzhe
+ * @since 10/12/18
+ */
+public class OSExecuteException extends RuntimeException {
+    public OSExecuteException(String s) {
+        super(s);
+    }
+}
