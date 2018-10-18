e35b083b9938c7fd9b744c8619569b85735f86d6
yuki <cuiyuzhe@miaozhen.com>
2018-10-18 18:17:18 +0800

    [feature]ch 19

--- src/java/code/ch18/Person.java
+++ src/java/code/ch18/Person.java
@@ -1,6 +1,15 @@
 package code.ch18;
 
+import nu.xom.Document;
 import nu.xom.Element;
+import nu.xom.Serializer;
+import org.apache.commons.compress.utils.CharsetNames;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author yuzhe
@@ -14,7 +23,7 @@ public class Person {
         this.last = l;
     }
 
-    public Element getXML(){
+    public Element getXML() {
         Element person = new Element("person");
         Element firstName = new Element("first");
         firstName.appendChild(first);
@@ -24,4 +33,40 @@ public class Person {
         person.appendChild(lastName);
         return person;
     }
+
+    public Person(Element person) {
+        first = person.getFirstChildElement("first").getValue();
+        last = person.getFirstChildElement("last").getValue();
+    }
+
+    @Override
+    public String toString() {
+        return first + " " + last;
+    }
+
+    public static void format(OutputStream out, Document doc) throws Exception {
+        Serializer serializer = new Serializer(out, CharsetNames.UTF_8);
+        serializer.setIndent(4);
+        serializer.setMaxLength(60);
+        serializer.write(doc);
+        serializer.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<Person> people = Arrays.asList(
+                new Person("Dr a", "b"),
+                new Person("c", "d"),
+                new Person("e", "f"),
+                new Person("g", "h")
+        );
+        System.out.println(people);
+        Element root = new Element("people");
+        for (Person p : people) {
+            root.appendChild(p.getXML());
+        }
+        Document doc = new Document(root);
+        format(System.out, doc);
+        format(new BufferedOutputStream(new FileOutputStream("people.xml")), doc);
+    }
+
 }
---
+++ src/java/code/ch18/PreferencesDemo.java
@@ -0,0 +1,27 @@
+package code.ch18;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+/**
+ * @author yuzhe
+ * @since 10/18/18
+ */
+public class PreferencesDemo {
+
+    public static void main(String[] args) throws BackingStoreException {
+        Preferences prefs = Preferences.userNodeForPackage(PreferencesDemo.class);
+        prefs.put("location", "oz");
+        prefs.put("footwear", "slippers");
+//        prefs.putInt("companions", 4);
+        prefs.putBoolean("are there witches", false);
+
+        int usageCount = prefs.getInt("usage count", 0);
+        prefs.putInt("usage count", usageCount);
+        for (String s : prefs.keys()) {
+            System.out.println(s + ":" + prefs.get(s, null));
+        }
+        System.out.println("how many companions does dorothy have? " + prefs.getInt("companions", 0));
+    }
+
+}
