3b592818131eff6caa0b2ec42a1784db7aa48e62
yuki <cuiyuzhe@miaozhen.com>
2018-10-18 19:22:55 +0800

    [wip]alter constructure

--- src/main/java/ch19/EnumClass.java
+++ src/main/java/ch19/EnumClass.java
@@ -4,5 +4,28 @@ package ch19;
  * @author yuzhe
  * @since 10/18/18
  */
+enum Shrubbery {
+    GROUND,
+    CRAWLING,
+    HANGING
+}
+
+
 public class EnumClass {
+    public static void main(String[] args) {
+        for (Shrubbery s : Shrubbery.values()) {
+            System.out.println(s + ".ordinal = " + s.ordinal());
+            System.out.println(s + ".compareTo(" + Shrubbery.GROUND + ") = " + s.compareTo(Shrubbery.GROUND));
+            System.out.println(s + " == " + Shrubbery.HANGING + " = " + (s == Shrubbery.HANGING));
+            System.out.println(s.getDeclaringClass());
+            System.out.println(s.name());
+            System.out.println("--------------------------------------");
+        }
+        for (String s : "GROUND CRAWLING HANGING".split(" ")) {
+            Shrubbery shrubbery = Shrubbery.valueOf(s);
+            System.out.println(shrubbery);
+        }
+
+    }
+
 }
