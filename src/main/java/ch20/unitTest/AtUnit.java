d8a6aca89279419758be9df1cf3b3e7eb1cf0ffb
yuki <cuiyuzhe@miaozhen.com>
2018-11-08 19:43:11 +0800

    [wip]at unit

--- src/main/java/ch20/unitTest/AtUnit.java
+++ src/main/java/ch20/unitTest/AtUnit.java
@@ -1,6 +1,7 @@
 package ch20.unitTest;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,9 +18,35 @@ public class AtUnit implements ProcessFiles.Strategy {
 
     @Override
     public void process(File file) {
-        try{
-        } catch (Exception e){
+        try {
+            String cName = ClassNameFinder.thisClass(BinaryFile.read(file));
+            if (cName.contains(".")) {
+                return;
+            } else {
+                testClass = Class.forName(cName);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        TestMethods testMethods = new TestMethods();
+        Method creator = null;
+        Method cleanup = null;
+        for (Method m : testClass.getDeclaredMethods()) {
 
+        }
+
+    }
+
+    static class TestMethods extends ArrayList<Method> {
+        void addIfTestMethod(Method m){
+            if(m.getAnnotation(Test.class) == null){
+                return;
+            }
+            if(!(m.getReturnType().equals(boolean.class) || m.getReturnType().equals(void.class))){
+                throw new RuntimeException("@Test method must return boolean or void type");
+            }
+            m.setAccessible(true);
+            add(m);
         }
     }
 }
